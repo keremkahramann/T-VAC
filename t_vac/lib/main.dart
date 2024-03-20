dd0f0194b79c3cfa50599ba0d13ade3411c530da
Kerem Kahraman <keremkahramann02@gmail.com>
2024-03-21 02:54:41 +0300

    Persistent bottom navbar setup

--- t_vac/lib/main.dart
+++ t_vac/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quickalert/quickalert.dart';
+// import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +19,42 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
+    //  PersistentTabController controller;
+
+    // controller = PersistentTabController(initialIndex: 0);
+
+    // return PersistentTabView(
+    //   context,
+    //   screens: [],
+    //   controller: controller,
+    //   confineInSafeArea: true,
+    //   backgroundColor: Colors.white, // Default is Colors.white.
+    //   handleAndroidBackButtonPress: true, // Default is true.
+    //   resizeToAvoidBottomInset:
+    //       true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+    //   stateManagement: true, // Default is true.
+    //   hideNavigationBarWhenKeyboardShows:
+    //       true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+    //   decoration: NavBarDecoration(
+    //     borderRadius: BorderRadius.circular(10.0),
+    //     colorBehindNavBar: Colors.white,
+    //   ),
+    //   popAllScreensOnTapOfSelectedTab: true,
+    //   popActionScreens: PopActionScreensType.all,
+    //   itemAnimationProperties: ItemAnimationProperties(
+    //     // Navigation Bar's items animation properties.
+    //     duration: Duration(milliseconds: 200),
+    //     curve: Curves.ease,
+    //   ),
+    //   screenTransitionAnimation: ScreenTransitionAnimation(
+    //     // Screen transition animation on change of selected tab.
+    //     animateTabTransition: true,
+    //     curve: Curves.ease,
+    //     duration: Duration(milliseconds: 200),
+    //   ),
+    //   navBarStyle:
+    //       NavBarStyle.style1, // Choose the nav bar style with this property.
+    // );
   }
 }
 
@@ -31,8 +68,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   void _incrementCounter() {
     // setState(() {
     //   _counter++;
@@ -47,16 +82,12 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'You have pushed the button this many times:',
             ),
           ],
         ),
