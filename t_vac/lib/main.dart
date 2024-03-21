ea3c6bffb66b508e7bfe74c1e7cbb9848889952f
tahaglbz <tahaglbz1@gmail.com>
2024-03-21 17:17:05 +0300

    firebase initialize , firestore and auth packages

--- t_vac/lib/main.dart
+++ t_vac/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:quickalert/quickalert.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:t_vac/firebase_options.dart';
 // import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
